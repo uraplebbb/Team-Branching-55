cc39f65f3c520e25442e1c832151453d24326f85
Ma afee Basimahgit config --global user.email 230248896@aston.ac.uk <230248896@aston.ac.uk>
2025-10-21 23:29:15 +0100

    Added Max function by Ma-afee

---
+++ Math.java
@@ -0,0 +1,10 @@
+/********************************
+Developer: Ma-afee
+University ID: AST123456
+Function: This function returns the maximum of two integers
+********************************/
+public class Math {
+    public int Max(int x, int y) {
+        return (x > y) ? x : y;
+    }
+}
