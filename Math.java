4ddab75849f8efdd64168abf484c5131adc46bf5
Thomas Harvey <240168801@aston.ac.uk>
2025-10-24 12:05:59 +0100

    Implemented 'add' function into Math Class

--- Math.java
+++ Math.java
@@ -7,4 +7,21 @@ public class Math {
     public int Max(int x, int y) {
         return (x > y) ? x : y;
     }
+
+
+
+    /*******************************
+
+	Developer: Tom Harvey
+	UniversityID: 240168801
+	Function: This function will return the sum of two integers
+
+    *******************************/
+
+    public int add(int n1, int n2) {
+
+	return n1 + n2;
+
+    }
+
 }
